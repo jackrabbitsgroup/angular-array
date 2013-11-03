d60ee9545145fc6fa3d3130738c0cc8d9d3e881c
Luke Madera <luke.madera@gmail.com>
2013-11-03 09:13:53 -0800

    first build

--- pages/home/HomeCtrl.js
+++ pages/home/HomeCtrl.js
@@ -4,5 +4,16 @@
 'use strict';
 
 angular.module('myApp').controller('HomeCtrl', ['$scope', 'jrgArray', function($scope, jrgArray) {
-	//TODO - put any directive code here
+	$scope.arr1 =[
+		{_id:1, name:'Joe'},
+		{_id:2, name:'Bob'},
+		{_id:3, name:'Sally'},
+		{_id:4, name:'Sue'},
+		{_id:5, name:'Becky'}
+	];
+	$scope.index1 =jrgArray.findArrayIndex($scope.arr1, 'name', 'Bob', {});
+	
+	$scope.arrSorted =jrgArray.sort2D($scope.arr1, 'name', {});
+	
+	$scope.arrRemove =jrgArray.remove($scope.arr1, 1);
 }]);
