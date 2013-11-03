e0a55e4333914e13b1998b114c46d9af329e7c22
Luke Madera <luke.madera@gmail.com>
2013-11-03 07:54:01 -0800

    'init'

---
+++ array.js
@@ -0,0 +1,19 @@
+/**
+@fileOverview
+
+@toc
+
+*/
+
+'use strict';
+
+angular.module('jackrabbitsgroup.angular-array', [])
+.factory('jrgArray', [ function () {
+	//methods and properties (some of which may be private - explicitly state which methods and properties to expose/return at bottom)
+	var privateObj ={
+	};
+	
+	//select which methods/functions (and potentially properties) to expose
+	return {
+	};
+}]);
---
+++ pages/home/HomeCtrl.js
@@ -0,0 +1,8 @@
+/**
+*/
+
+'use strict';
+
+angular.module('myApp').controller('HomeCtrl', ['$scope', 'jrgArray', function($scope, jrgArray) {
+	//TODO - put any directive code here
+}]);
